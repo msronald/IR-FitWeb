65a074ecd24b824caf207f5c8f178a4f1b1fd485
ronald <ronald.munayco@unmsm.edu.pe>
2025-06-29 18:17:40 -0500

    Login de usuarios con api a Base de datos + productos(sin acceso a base de datos)

---
+++ admin/vite-project/src/pages/Orders/Orders.jsx
@@ -0,0 +1,84 @@
+import React, { useEffect, useState } from 'react'
+import "./Orders.css"
+import axios from 'axios';
+import { toast } from 'react-toastify';
+import {assets} from '../../assets/assets';
+const Orders = ({url}) => {
+
+  const[orders, setOrders] = useState([]);
+
+  //Toma y guarda las ordenes en un arreglo comunicandose con la base de datos
+  const fetchOrders = async () => {
+    const response = await axios.get(url+"/api/order/list");
+    if(response.data.success){
+      setOrders(response.data.data);
+      console.log(response.data.data);
+    }
+    else{
+      alert("Error al cargar las ordenes");
+      toast.error("Error al cargar las ordenes");
+    }
+  }
+
+  //El estado del objeto cambia en la base de datos
+  const statusHandler = async (event, orderId) => {
+   const response = await axios.post(url+"/api/order/status", {
+    orderId,
+    status: event.target.value
+   });
+    if(response.data.success){
+        await fetchOrders();
+    }else{
+      toast.error("Error al actualizar el estado de la orden");
+    }
+  }
+
+  useEffect(() => {
+    fetchOrders();
+  }, []);
+
+  return (
+    <div className="order add ">
+      <h3>Lista de Ordenes</h3>
+      <div className="order-list">
+        {orders.map((order, index) => ( 
+        <div key={index} className="order-item">{/* traemos las ordenes de la base de datos  y  en el div agregamos una key para que no de error */}
+        <img src={assets.parcel_icon} alt=''/> {/* traemos las imagenes de la carpeta assets */}
+              <div>
+                    <p className="order-item-food">
+                      {/* traemos los items de la orden y los mostramos en pantalla */}
+                      {order.items.map((item, index) => {
+                        if(index === order.items.length - 1){
+                          return item.name + " x" + item.quantity;
+                        }else{
+                          return item.name + " x" + item.quantity + ", ";
+                        }
+                      })}
+                    </p>
+                   
+                    <p className='order-item-name'>
+                      {order.address.firstName+" "+ order.address.lastName} {/* traemos el nombre del usuario */}  
+                    </p>
+                    <div className='order-item-address'>
+                      <p>{order.address.street +","}</p>
+                      <p>{order.address.city+", "+ order.address.state+ ", "+ order.address.country+", "+order.address.zipcode}</p>
+                    </div>
+                    <p className='order-item-phone'>
+                        {order.address.phone} {/* traemos el telefono del usuario */}
+                        </p>
+            </div>
+          <p>Items: {order.items.length}</p>
+          <p>Precio Total: {order.amount} PEN</p>
+          <select onChange={(e) => statusHandler(e, order._id)} value={order.status} className="order-item-status"> {/* traemos el estado de la orden y creamos un evento que se ejecute al cambiar el estado */}
+                      <option value="Procesando">En proceso</option>
+                      <option value="Pendiente">Pendiente</option>
+                      <option value="Entregado">Entregado</option>
+          </select>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Orders
--- frontend/src/componentes/LoginPopup/LoginPopup.jsx
+++ frontend/src/componentes/LoginPopup/LoginPopup.jsx
@@ -1,25 +1,67 @@
 import React, { useState } from 'react';
 import './LoginPopup.css'
 import { assets } from '../../assets/assets'
+import { useContext } from 'react';
+import { TiendaContext } from '../../context/TiendaContext';
+import { useEffect } from 'react';
+import axios from 'axios'
 
 const LoginPopup = ({setShowLogin}) => {
 
+    const {url, setToken} = useContext(TiendaContext)
     const [currState,setCurrState] = useState("Iniciar sesión")
+    const [data, setData] = useState({
+        nombre: "",
+        email: "",
+        contraseña: ""
+    })
+
+    const onChangeHandler = (event) =>{ //Maneja el evento donde los campos cambian su contenido y actualiza el objeto data
+        const name = event.target.name;
+        const value = event.target.value;
+        setData(data=>({...data, [name]:value})) //añade un campo name de valor value tal que name: value
+    }
+
+    useEffect(()=>{
+        console.log(data)
+    },[data])
+
+    const onLogin = async (event) =>{
+        event.preventDefault()
+        let newUrl = url;
+        if(currState==="Login"){
+            newUrl += "/api/user/login"
+        }
+        else{
+            newUrl += "/api/user/register"
+        }
+
+        const response = await axios.post(newUrl, data);
+
+        if(response.data.success){
+            setToken(response.data.token); //Token de la API backend
+            localStorage.setItem("token", response.data.token);
+            setShowLogin(false)
+        }
+        else{
+            alert(response.data.message)
+        }
+    }
 
   return (
     <div className = "login-popup">
-        <form className="login-popup-container">
+        <form onSubmit={onLogin} className="login-popup-container">
             <div className="login-popup-title">
                 <h2>{currState}</h2>
                 <img onClick={()=>setShowLogin(false)} src={assets.cross_icon} alt="" />
             </div>
             <div className="login-popup-inputs">
-                {currState==="Iniciar sesión"?<></>: <input type="text" placeholder='Tu nombre' required />}
+                {currState==="Iniciar sesión"?<></>: <input name='nombre' onChange={onChangeHandler} value={data.nombre} type="text" placeholder='Tu nombre' required />}
                
-                <input type="email" placeholder='Tu Correo' required />
-                <input type="password" placeholder='Contraseña' required />
+                <input name='email' onChange={onChangeHandler} value={data.email} type="email" placeholder='Tu Correo' required />
+                <input type="contraseña" onChange={onChangeHandler} value={data.contraseña} placeholder='Contraseña' required />
             </div>
-            <button>{currState==="Registrarse"?"Crear cuenta":"Iniciar sesión"}</button>
+            <button type='submit'>{currState==="Registrarse"?"Crear cuenta":"Iniciar sesión"}</button>
             <div className="login-popup-condition">
                 <input type="checkbox" required />
                 <p>
